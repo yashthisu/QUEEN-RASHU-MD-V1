b2a643cb837e17ec8a91d48c9d018e7a56274ea5
NipunHarshana1119 <queenrashumd@gmail.com>
2025-02-03 22:44:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝐐𝐔𝐄𝐄𝐍-𝐑𝐀𝐒𝐇𝐔-𝐌𝐃=AoJm3QbJ#06W4gpiRh0Xg89ROmTymuYtC1LL22_ZGtqMWwc2H_Ss",
-MONGODB: process.env.MONGODB || "mongodb://mongo:sgYeENbATaJvEgIlkSRUeFVpfRVjjXGt@autorack.proxy.rlwy.net:28022",
+SESSION_ID: process.env.SESSION_ID || "Past Your Session Id 🍃",
+//උඩ ☝️ තියෙන Past Sesion Id කියන එක අයින් කරලා ඔයාගෙ Sesion Id එක Past කරන්න 😁
+MONGODB: process.env.MONGODB || "Past Your MongoDB Url 🍃",
+//උඩ ☝️ තියෙන Past MongoDB Url කියන එක අයින් කරලා ඔයාගෙ MongoDB Url එක Past කරන්න 😁
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/BsjkCDP/9555.jpg",
 BOT_NAME: process.env.BOT_NAME || "QUEEN-RASHU-MD",
 LANG: process.env.BOT_LANG || 'EN' ,
